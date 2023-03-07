3b9ec75f80617f5cecb985dbf635448d3850a4cc
kadek12345 <kadek.sucipta005@gmail.com>
2023-03-07 13:23:12 +0800

    sendfile

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use("/api", skillRoute);
 
 //home
 app.use("/", function(req, res) {
-  res.render("./index.html", {
+  res.sendFile("./index.html", {
     title: "Kadek API Service (Error Page)"
   })
 })
