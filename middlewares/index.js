69cf84b37c9d9e315a5898cdd8cba54d89e3df02
kadek12345 <kadek.sucipta005@gmail.com>
2023-03-07 14:55:43 +0800

    middlewares

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,52 +1,52 @@
-// const {getToken, policyFor} = require("../utils");
-// const jwt = require("jsonwebtoken")
-// const config = require("../app/config")
+const {getToken, policyFor} = require("../utils");
+const jwt = require("jsonwebtoken")
+const config = require("../app/config")
 
-// function decodeToken() {
-//     return async function(req, res, next) {
-//         try {
-//             let token = getToken(req);
+function decodeToken() {
+    return async function(req, res, next) {
+        try {
+            let token = getToken(req);
 
-//             if(!token) return next();
+            if(!token) return next();
 
-//             req.user = jwt.verify(token, config.secretkey);
+            req.user = jwt.verify(token, config.secretkey);
 
-//             let user = await User.findOne({token: {$in: [token]}});
+            let user = await User.findOne({token: {$in: [token]}});
 
-//             if(!user) {
-//                 res.json({
-//                     error: 1,
-//                     message: "Token Expired"
-//                 });
-//             }
-//         } catch (err) {
-//             if(err && err.name === "JsonWebTokenError") {
-//                 return res.json({
-//                     error: 1,
-//                     message: err.message
-//                 });
-//             }
-//             next(err)
-//         }
-//         return next();
-//     }
-// }
+            if(!user) {
+                res.json({
+                    error: 1,
+                    message: "Token Expired"
+                });
+            }
+        } catch (err) {
+            if(err && err.name === "JsonWebTokenError") {
+                return res.json({
+                    error: 1,
+                    message: err.message
+                });
+            }
+            next(err)
+        }
+        return next();
+    }
+}
 
-// //middleware untuk cek hak akses
-// function police_check(action, subject) {
-//     return function(req, res, next) {
-//         let policy = policyFor(req.user);
-//         if(!policy.can(action, subject)) {
-//             return res.json({
-//                 error: 1,
-//                 message: `Your are not allowed to ${action} ${subject}`
-//             });
-//         }
-//         next()
-//     }
-// }
+//middleware untuk cek hak akses
+function police_check(action, subject) {
+    return function(req, res, next) {
+        let policy = policyFor(req.user);
+        if(!policy.can(action, subject)) {
+            return res.json({
+                error: 1,
+                message: `Your are not allowed to ${action} ${subject}`
+            });
+        }
+        next()
+    }
+}
 
-// module.exports = {
-//     decodeToken,
-//     police_check 
-// }
+module.exports = {
+    decodeToken,
+    police_check 
+}
